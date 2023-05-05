a027dcf24faedd12e9bde8596aea4bc77adb6344
kosy0907 <syko1234@naver.com>
2023-05-06 00:05:35 +0900

    modify

--- src/component/kakaoScript.js
+++ src/component/kakaoScript.js
@@ -1,4 +1,4 @@
-import react, { useEffect, useMemo } from "react";
+import react, { useEffect, useMemo, useState } from "react";
 import styled from "styled-components";
 import { elevatorLocation } from "../data/elevatorLocation";
 const { kakao } = window;
@@ -9,6 +9,8 @@ const MapDiv = styled.div`
 `
 
 export default function KakaoMapScript({ searchText }) {
+    const [map, setMap] = useState(null);
+
     const markers = useMemo(() => {
         const markers = [];
         for (var i = 0; i < elevatorLocation.DATA.length; i++) {
@@ -32,59 +34,63 @@ export default function KakaoMapScript({ searchText }) {
     }, []);
 
     useEffect(() => {
-        // kakao map
-        const container = document.getElementById('kakao-map');
-        const options = {
-            center: new kakao.maps.LatLng(37.566826, 126.9786567),
-            level: 3
-        };
-        const map = new kakao.maps.Map(container, options);
+        kakao.maps.load(() => {
+            // kakao map
+            const container = document.getElementById('kakao-map');
+            const options = {
+                center: new kakao.maps.LatLng(37.566826, 126.9786567),
+                level: 3
+            };
+            const newMap = new kakao.maps.Map(container, options);
+            setMap(newMap);
 
-        for (var j = 0; j < markers.length; j++) {
-            markers[j].setMap(map);
-        }
-
-        const getCurrentPos = () => {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(
-                    function (position) {
-                        var lat = position.coords.latitude;
-                        var lon = position.coords.longitude;
-                        var currentPos = new kakao.maps.LatLng(lat, lon);
-                        var marker = new kakao.maps.Marker({
-                            map: map,
-                            position: currentPos,
-                            image: new kakao.maps.MarkerImage(
-                                "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
-                                new kakao.maps.Size(24, 35),
-                                { offset: new kakao.maps.Point(13, 35) }
-                            )
-                        });
-                        map.setCenter(currentPos);
-                    },
-                    function (err) {
-                        options.center = new kakao.maps.LatLng(37.566826, 126.9786567);
-                        const map = new kakao.maps.Map(container, options);
-                    })
+            for (var j = 0; j < markers.length; j++) {
+                markers[j].setMap(newMap);
             }
-        }
-        getCurrentPos();
 
-        const filteredLocations = elevatorLocation.DATA.filter(
-            (location) => location.sw_nm === searchText
-        );
-
-        if (filteredLocations.length > 0) {
-            const regex = /\d+\.\d+/g;
-            const filteredPoint = filteredLocations[0].node_wkt.match(regex);
-            console.log(filteredPoint);
-            const moveLotation = new kakao.maps.LatLng(parseFloat(filteredPoint[1]), parseFloat(filteredPoint[0]));
-            console.log(moveLotation);
-            kakao.maps.event.addListener(map, "tilesloaded", function () {
-                map.panTo(moveLotation);
+            const getCurrentPos = () => {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(
+                        function (position) {
+                            var lat = position.coords.latitude;
+                            var lon = position.coords.longitude;
+                            var currentPos = new kakao.maps.LatLng(lat, lon);
+                            var marker = new kakao.maps.Marker({
+                                map: newMap,
+                                position: currentPos,
+                                image: new kakao.maps.MarkerImage(
+                                    "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
+                                    new kakao.maps.Size(24, 35),
+                                    { offset: new kakao.maps.Point(13, 35) }
+                                )
+                            });
+                            newMap.setCenter(currentPos);
+                        },
+                        function (err) {
+                            options.center = new kakao.maps.LatLng(37.566826, 126.9786567);
+                            const newMap = new kakao.maps.Map(container, options);
+                            setMap(newMap);
+                        })
+                }
             }
-            )
-        }
+            getCurrentPos();
+
+            const filteredLocations = elevatorLocation.DATA.filter(
+                (location) => location.sw_nm === searchText
+            );
+
+            if (filteredLocations.length > 0) {
+                const regex = /\d+\.\d+/g;
+                const filteredPoint = filteredLocations[0].node_wkt.match(regex);
+                console.log(filteredPoint);
+                const moveLotation = new kakao.maps.LatLng(parseFloat(filteredPoint[1]), parseFloat(filteredPoint[0]));
+                console.log(moveLotation);
+                kakao.maps.event.addListener(newMap, "tilesloaded", function () {
+                    newMap.panTo(moveLotation);
+                }
+                )
+            }
+        })
     }, [searchText, markers]);
 
     return (
